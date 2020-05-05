225b4d27b49f1f335f7a418c47885d75a13769f2
Ernest Jewczyn <eje@webmen.de>
2020-05-05 15:09:52 +0200

    Car Klass mit getter und setter

--- src/logic/Car.java
+++ src/logic/Car.java
@@ -9,4 +9,20 @@ public class Car {
         this.mileAge = mileAge;
         this.actualSpeed = actualSpeed;
     }
+
+    public String getMileAge() {
+        return mileAge;
+    }
+
+    public String getActualSpeed() {
+        return actualSpeed;
+    }
+
+    public void setMileAge(String mileAge) {
+        this.mileAge = mileAge;
+    }
+
+    public void setActualSpeed(String actualSpeed) {
+        this.actualSpeed = actualSpeed;
+    }
 }

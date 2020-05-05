ca01fbffc472cada2e5750f6953f9f4a69067840
Ernest Jewczyn <eje@webmen.de>
2020-05-05 14:21:35 +0200

    Klasse Car hinzugefuegt

---
+++ src/logic/Car.java
@@ -0,0 +1,12 @@
+package logic;
+
+public class Car {
+
+    String mileAge;
+    String actualSpeed;
+
+    public Car(String mileAge, String actualSpeed) {
+        this.mileAge = mileAge;
+        this.actualSpeed = actualSpeed;
+    }
+}
